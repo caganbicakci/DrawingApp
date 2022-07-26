3517d6c31582e73aedb142f5aadae2f2e10df3ac
caganbicakci <caganbicakci97@gmail.com>
2022-07-26 09:47:45 +0300

    brush dialog added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,35 @@ class MainActivity : AppCompatActivity() {
 
         binding.drawingView.setBrushSize(10f)
 
+        binding.brushIb.setOnClickListener{
+            showBrushSizeDialog()
+        }
+
+    }
+
+    private fun showBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.brush_size_dialog)
+        brushDialog.setTitle("Brush size")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.small_brush_btn)
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.medium_brush_btn)
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.large_brush_btn)
+
+        smallBtn.setOnClickListener(){
+            binding.drawingView.setBrushSize(10f)
+            brushDialog.dismiss()
+        }
+
+        mediumBtn.setOnClickListener(){
+            binding.drawingView.setBrushSize(15f)
+            brushDialog.dismiss()
+        }
+
+        largeBtn.setOnClickListener(){
+            binding.drawingView.setBrushSize(20f)
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
